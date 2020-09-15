d9e50df40b4b813ae5c36db1933dca8b39c2af5b
RISHABH MISHRA <dmrishabh@users.noreply.github.com>
2020-09-15 12:28:52 +0530

    Version 1

---
+++ app/src/main/java/com/rishabh/ecocarz/MainActivity.java
@@ -0,0 +1,48 @@
+package com.rishabh.ecocarz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/ECOCARZ");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+
+
+    public class webClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()) {
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
